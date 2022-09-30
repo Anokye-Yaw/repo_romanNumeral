8d1478be35dc476a778df429ce005a8857a0f0fc
Anokye-Yaw <106571751+Anokye-Yaw@users.noreply.github.com>
2022-09-30 06:26:59 +0000

    first commit

---
+++ romanNumeral.js
@@ -0,0 +1,15 @@
+function convertToRomanNumeral(num){
+const roman = ["M","CM","D","CD","C","XC","L","XL","X","IX","IV","I"];
+const number = [1000,900,500,400,100,90,50,40,10,9,4,1];
+   
+   let result = "";
+   for(let i=0;i<roman.length;i++){
+      while(num >= number[i]){  
+       result += roman[i];
+       num -= number[i];
+
+   }   
+ }
+    return result;
+}
+convertToRomanNumeral(400);
