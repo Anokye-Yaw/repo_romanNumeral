8323ee8d7193739379c9d05f1d30c58eb6b705a7
Anokye Yaw  Agyekum <anokyeyaw53@gmail.com>
2022-09-30 06:34:16 +0000

    Update romanNumeral.js

--- romanNumeral.js
+++ romanNumeral.js
@@ -1,6 +1,6 @@
 function convertToRomanNumeral(num){
-const roman = ["M","CM","D","CD","C","XC","L","XL","X","IX","IV","I"];
-const number = [1000,900,500,400,100,90,50,40,10,9,4,1];
+const roman = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+const number = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
    
    let result = "";
    for(let i=0;i<roman.length;i++){
